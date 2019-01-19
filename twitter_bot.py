343d8553e403cd4127a94a96c8abf5964450b52c
Daniel Tuveson <danieltuveson@gmail.com>
2019-01-18 22:37:25 -0800

    removed unused imports

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,20 +1,18 @@
 import tweepy
-import random
 from keys import keys
-from time import localtime, strftime, sleep
 from os import listdir
  
 CONSUMER_KEY = keys['consumer_key'] 
 CONSUMER_SECRET = keys['consumer_secret'] 
 ACCESS_KEY = keys['access_token']
 ACCESS_SECRET = keys['access_token_secret']
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+AUTH.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+API = tweepy.API(AUTH)
 
 if __name__ == '__main__':
     photos = listdir('./photos')
     for photo in photos:
-        api.update_with_media(f'./photos/{photo}')
+        API.update_with_media(f'./photos/{photo}')
         sleep(60 * 60 * 24) # Sleeps for one day
-    api.update_status('Welp I\'m out of photos. Hopefully I\'ll have some more soon')
+    API.update_status('Welp I\'m out of photos. Hopefully I\'ll have some more soon')
