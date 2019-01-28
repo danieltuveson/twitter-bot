1565d2c8c525cb508981a8d6b646c48acbef1a90
Daniel Tuveson <danieltuveson@gmail.com>
2019-01-28 12:02:02 -0800

    added scheduler instead of using 'wait' to run bot, bot is now deployed as a clock instead of a worker

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,18 +1,28 @@
 import tweepy
-from keys import keys
-from os import listdir
+from os import listdir, environ
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import date, timedelta
+
  
-CONSUMER_KEY = keys['consumer_key'] 
-CONSUMER_SECRET = keys['consumer_secret'] 
-ACCESS_KEY = keys['access_token']
-ACCESS_SECRET = keys['access_token_secret']
+CONSUMER_KEY = os.environ.get('consumer_key')
+CONSUMER_SECRET = os.environ.get('consumer_secret') 
+ACCESS_KEY = os.environ.get('access_token')
+ACCESS_SECRET = os.environ.get('access_token_secret')
 AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 AUTH.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 API = tweepy.API(AUTH)
+sched = BackgroundScheduler()
 
-if __name__ == '__main__':
-    photos = listdir('./photos')
-    for photo in photos:
-        API.update_with_media(f'./photos/{photo}')
-        sleep(60 * 60 * 24) # Sleeps for one day
-    API.update_status('Welp I\'m out of photos. Hopefully I\'ll have some more soon')
+
+def post_status(photo):
+    API.update_with_media(f'./photos/{photo}')
+
+
+date = date(2019, 1, 28)
+photos = listdir('./photos')
+interval = timedelta(days=1)
+for photo in photos: 
+    sched.add_job(post_status, 'date', run_date=date, args=[photo])
+    date += interval
+
+sched.start()
